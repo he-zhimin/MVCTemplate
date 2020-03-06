03fb38deac7231bb804192f277690f1641a3a7e7
‘ZhangQuequegit config --global user.email 3393597524@qq.comgit config --global user.name ‘ZhangQueque <3393597524@qq.com>
2020-03-07 01:21:13 +0800

    添加项目文件。

---
+++ MVC/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult UserCenter()
+        {
+            return View();
+        }
+    }
+}
