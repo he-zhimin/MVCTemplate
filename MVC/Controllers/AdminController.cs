478835e3e41b6a261d84529b68bf53c1b30e58be
‘ZhangQuequegit config --global user.email 3393597524@qq.comgit config --global user.name ‘ZhangQueque <3393597524@qq.com>
2020-03-14 18:52:00 +0800

    全部完成，只差一个权限的树了

---
+++ MVC/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserIndex()
+        {
+            return View();
+        }
+        public IActionResult RoleIndex()
+        {
+            return View();
+        }
+
+        public IActionResult ModuleIndex()
+        {
+            return View();
+        }
+
+        public IActionResult DutyIndex()
+        {
+            return View();
+        }
+
+    }
+}
